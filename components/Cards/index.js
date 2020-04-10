318912a4bccc6280fbeeea31c09375832dcfc780
Justin Lohner <lohner4@gmail.com>
2020-04-10 10:09:52 -0600

    wrote the cardMaker function for the article cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,50 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function cardMaker(articleData) {
+
+	// create elements
+	let card = document.createElement('div');
+	let headLine = document.createElement('div');
+	let author = document.createElement('div');
+	let imgContainer = document.createElement('div');
+	let authorImg = document.createElement('img');
+	let authorName = document.createElement('span');
+
+	// structure
+	card.appendChild(headLine);
+	card.appendChild(author);
+	author.appendChild(imgContainer);
+	author.appendChild(authorName);
+	imgContainer.appendChild(authorImg);
+
+	// classes
+	card.classList.add('card');
+	headLine.classList.add('headLine');
+	author.classList.add('author');
+	imgContainer.classList.add('img-container');
+
+	// content
+	headLine.textContent = articleData.headline;
+	authorImg.setAttribute('src', articleData.authorPhoto);
+	authorName.textContent = articleData.authorName;
+
+	return card;
+}
+
+let object = {
+	headline: "ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects",
+	authorPhoto: "./assets/sir.jpg",
+	authorName: "SIR RUFF'N'STUFF"
+};
+
+console.log(cardMaker(object));
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+// 	.then(r => {
+// 		console.log(r);
+// 	})
+// 	.catch(e => {
+// 		console.log('error');
+// 	})
